e1f314cca51c25a4cf15b84738a8fe7bdbb1b976
prythagoras <prithivimaruthachalam@gmail.com>
2020-12-16 15:56:37 +0530

    Kitchen responsiveness and customisedInt fixes

--- src/components/home/kitchen.js
+++ src/components/home/kitchen.js
@@ -11,12 +11,16 @@ class CustomisedInt extends Component{
         return(
             <div className="fluid-container kitchen-base">
                 <div className="kitchen-A">
-                    <img className="kitchen-img mr-img" src={kitchenImageA} alt="Modular Kitchens in Coimbatore, Erode, Ooty, Salem, Tamil Nadu Acrylic Kitchen Shutters Coimbatore, Erode, Ooty, Salem, Tamil Nadu"/>
-                    <img className="kitchen-img" src={kitchenImageB} alt="Customized Modular kitchens german engineering sleek kitchens Made in India"/>
+                    <div>
+                        <img className="kitchen-img img-fluid" src={kitchenImageA} alt="Modular Kitchens in Coimbatore, Erode, Ooty, Salem, Tamil Nadu Acrylic Kitchen Shutters Coimbatore, Erode, Ooty, Salem, Tamil Nadu"/>
+                    </div>
+                    <div>
+                        <img className="kitchen-img img-fluid" src={kitchenImageB} alt="Customized Modular kitchens german engineering sleek kitchens Made in India"/>
+                    </div>
                 </div>
 
-                <div className="kitchen-B text-right">
-                    <div className="kitchen-title text-right">MODULAR KITCHENS</div>
+                <div className="kitchen-B">
+                    <div className="kitchen-title">MODULAR KITCHENS</div>
                     <div className="kitchen-content">
                         Here's a look at our functional, yet insanely sleek kitchens.
                         They're designed to fit your tastes &amp; crafted to perfection.
