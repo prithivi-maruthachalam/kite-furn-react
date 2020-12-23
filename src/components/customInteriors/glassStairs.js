6545e3ffe8d3584f5cfab953820b1c7d85c21d38
akshayar0811 <akshayar0811@gmail.com>
2020-12-23 14:41:05 +0530

    established folders in images and styles for glassstairs

--- src/components/customInteriors/glassStairs.js
+++ src/components/customInteriors/glassStairs.js
@@ -1,1 +1,17 @@
-//test
+import React, {Component} from 'react';
+
+import glassstairs1 from '../../media/images/custominteriors/glassstairs/glassstairs1.jpg';
+import glassstairs2 from '../../media/images/custominteriors/glassstairs/glassstairs2.jpg';
+
+import '../../styles/customInteriors/glassStairs.css';
+
+class glassStairs extends Component{
+    render(){
+        return(
+            <div className = "maindiv">
+                
+            </div>
+        );
+    }
+}
+export default glassStairs;
