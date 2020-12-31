c8c7550b02544524328e1274ec95b219b05744c9
prythagoras <prithivimaruthachalam@gmail.com>
2020-12-31 21:22:13 +0530

    Added complete responsiveness for process section

--- src/components/home/process.js
+++ src/components/home/process.js
@@ -6,7 +6,7 @@ class Process extends Component{
     render(){
         return(
             <div className="fluid-container process-base">
-                <div className="process-title text-left">
+                <div className="process-title">
                     So, how does it all work?
                 </div>
 
@@ -24,26 +24,26 @@ class Process extends Component{
                         </span>
                         <div className="process-puzzle-key process-color-A" />
                     </div>
-                    <div className="process-item process-color-A px-5">
+                    <div className="process-item process-color-A color-switch-A px-5">
                         <div className="process-pronoun">We</div>
                         <span id="p-3" className="p-text">
                             design and give a quote
                         </span>
-                        <div className="process-puzzle-key process-color-B" />
+                        <div className="process-puzzle-key process-color-B " id="disB" />
                     </div>
-                    <div className="process-item process-color-B px-5">
+                    <div className="process-item process-color-B color-switch-B px-5">
                         <div className="process-pronoun">We</div>
                         <span id="p-4" className="p-text">
                             co-ordinate with your civil contractor, plumber and everyone else involved
                         </span>
-                        <div className="process-puzzle-key process-color-A" />
+                        <div className="process-puzzle-key process-color-A" id="disA"/>
                     </div>
                     <div className="process-item process-color-A px-5">
                         <div className="process-pronoun">We</div>
                         <span id="p-5" className="p-text">
                             deliver and fit your new interior
                         </span>
-                        <div className="process-puzzle-key process-color-B" />
+                        <div className="process-puzzle-key process-color-B" id="disB"/>
                     </div>
                     <div className="process-item process-color-B px-5 borderB">
                         <span id="p-6">WE'RE DONE</span>
