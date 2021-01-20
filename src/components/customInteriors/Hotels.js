0a0180336e121cc6c8203cb2bcfd78ca42ad5806
prythagoras <prithivimaruthachalam@gmail.com>
2021-01-20 12:02:22 +0530

    Created hotels interiors base structure and added elements

--- src/components/customInteriors/Hotels.js
+++ src/components/customInteriors/Hotels.js
@@ -14,11 +14,47 @@ class Hotels extends Component{
             <div className="fluid-container hotels-base">
                 <div className="hotels-container">
                     <div className="hotels-grid-container">
-                        <div className="itemA">Header</div>
-                        <div className="itemB">imgA</div>
-                        <div className="itemC">imgB</div>
-                        <div className="itemD">imgC</div>
-                        <div className="itemE">imgD</div>
+                        <div className="itemA">
+                            <div>
+                                <div className="hotels-title">Hotel Interiors</div>
+                                <div className="hotels-content">
+                                    Lorem ipsum dolor sit amet, 
+                                    consectetur adipiscing elit. Fusce 
+                                    eu augue dolor. Mauris sodales 
+                                    pretium nisl, sit amet vulputate 
+                                    elit dictum in. Nam venenatis 
+                                    enim at cursus lobortis. 
+                                    Sed ut eros felis. 
+                                    Cras vulputate lorem 
+                                </div>
+                                <div className="hotels-links">
+                                    <a href="http://hotelatriumerode.com/">
+                                        <button className="btn-hotel-links">Hotel Atrium</button>
+                                    </a>
+                                    <a href="http://hotelatriumerode.com/">
+                                        <button className="btn-hotel-links">Hotel Blah</button>
+                                    </a>
+                                    <a href="http://hotelatriumerode.com/">
+                                        <button className="btn-hotel-links">Hotel 34sare12</button>
+                                    </a>
+                                    <a href="http://hotelatriumerode.com/">
+                                        <button className="btn-hotel-links">Hotel Ablion</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="itemB gridItem">
+                            <img src={A} alt=""/>
+                        </div>
+                        <div className="itemC gridItem">
+                            <img src={D} alt=""/>
+                        </div>
+                        <div className="itemD gridItem">
+                            <img src={C} alt=""/>
+                        </div>
+                        <div className="itemE gridItem">
+                            <img src={D} alt=""/>
+                        </div>
                     </div>
 
                     <img src={roomNumber} className="img-roomNumber" alt="" />
